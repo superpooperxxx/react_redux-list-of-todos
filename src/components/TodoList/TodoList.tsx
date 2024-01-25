a33efc6133e7d11a7ce12d4f4727ef69d446a128
Kyrylo Haiduk <superpooper@Kyrylos-MacBook-Pro-2.local>
2024-01-25 13:07:20 +0200

    solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable max-len */
 import React from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
